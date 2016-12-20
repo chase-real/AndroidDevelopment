85e4d1b239e42f62bc960e3973258d9ab2048d1a
chase-real <iakios@me.com>
2016-12-20 17:38:20 -0600

    help function for CommandDroid!
    Offers guidance on how to use CommandDroid!

---
+++ help.cpp
@@ -0,0 +1,17 @@
+//
+//  help.cpp
+//  PlayPin
+//
+//  Created by Chase Tillar on 12/19/16.
+//  Copyright © 2016 Chase Tillar. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void help() {
+    
+    cout<<"About: PlayPin! is a command line application with the aim of making Android devices easier to modify and tinker with. PlayPin! offers multiple deivce customization options as well as full ADB access to a number of Android devices. Hours spent searching and interpreting online threads for 'How-to' articles have turned into a few simple clicks!"<<endl<<endl;
+    cout<<"How to use: At the main menu you are greeted with the services supported by this application. Simply select the number of the action you would like to perform. From there you are greeted with a list of supported devices, simply select the number of your device. Click confirm action and sit back, we will let you know when your device is ready to go!"<<endl;
+}
